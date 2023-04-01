5ec674ce38f9cf68183f24083bcbfc837c998963
Sumayea Rahman <shorbonam104@gmail.com>
2023-04-01 20:32:45 +0600

    data array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('hi its ok');
+// console.log('hi its ok');
+const loadAi = () => {
+    const URl = 'https://openapi.programming-hero.com/api/ai/tools';
+
+    fetch(URl)
+    .then(res => res.json());
+    .then(data => console.log(data.data.tools));
+};
+loadAi();
