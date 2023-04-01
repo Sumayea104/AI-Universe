5bb9b5df1168ec6fb3bb1995350431293006043d
Sumayea Rahman <shorbonam104@gmail.com>
2023-04-01 20:38:59 +0600

    pass the data to a function

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ const loadAi = () => {
     const URl = 'https://openapi.programming-hero.com/api/ai/tools';
 
     fetch(URl)
-    .then(res => res.json());
-    .then(data => console.log(data.data.tools));
+        .then(res => res.json())
+        .then(data => showAi(data.data.tools));
 };
+
+const showAi = (data) => {
+    console.log(data);
+}
 loadAi();
