f3fd0c61d09f054655cc3392de21f28f687f9d60
Sumayea Rahman <shorbonam104@gmail.com>
2023-04-03 12:30:57 +0600

    ul & button

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const loadAi = () => {
     fetch(URl)
         .then(res => res.json())
         .then(data => showAi(data.data.tools));
-        
+
 };
 
 const showAi = (tools) => {
@@ -20,19 +20,18 @@ const showAi = (tools) => {
     <div  class="card w-full  bg-base-100 shadow-2xl">
             <img class="w-full h-64 px-10 pt-10" src="${tool.image}" alt="Shoes" class="rounded-xl" />
     <div class="card-body">
-    <h1 class="card-title">${tool}</h1>
-
-    <p >${tool.features}</p>
+    <h1 class="card-title">Features</h1>
+    <ol>
+        ${tool.features.map((feature, index) => `<li>${index + 1}. ${feature}</li>`).join('')}
+    </ol>
     <hr>
     <h1 class="card-title">${tool.name}</h1>
     <div class="flex g-4">
-                                <i class="fa-sharp fa-solid fa-calendar-days"></i>
-                                <p>${tool.published_in}</p>
-                            </div>
-            <button class="btn btn-primary ">
-                <i class="fa-solid fa-arrow-right"></i>
-            </button>
-        </div>
+            <i class="fa-sharp fa-solid fa-calendar-days"></i>
+            <p>${tool.published_in}</p>
+            <button class="btn btn-circle btn-outline btn-accent">
+        <i class="fa-solid fa-arrow-right"></i>
+        </button>
     </div>
     `
 
