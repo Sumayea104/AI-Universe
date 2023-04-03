4a4a59f5cb13cefa8a8547d90dea095ea1200ee4
Sumayea Rahman <shorbonam104@gmail.com>
2023-04-03 18:11:05 +0600

    modals

--- js/script.js
+++ js/script.js
@@ -25,9 +25,21 @@ const showAi = (tools) => {
                 <div class="flex g-4">
                     <i class="fa-sharp fa-solid fa-calendar-days"></i>
                     <p>${tool.published_in}</p>
-                    <button class="btn btn-circle btn-outline btn-accent ">
-                        <i class="fa-solid fa-arrow-right"></i>
-                    </button>
+                    
+<label for="my-modal-3" class="btn btn-circle btn-outline btn-accent">
+<i class="fa-solid fa-arrow-right"></i>
+</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal-3" class="modal-toggle" />
+<div class="modal">
+  <div class="modal-box relative">
+    <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+    <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+  </div>
+</div>
+                    
                 </div>
             </div>
         </div>
@@ -35,6 +47,7 @@ const showAi = (tools) => {
 
         toolContainer.appendChild(aiDiv);
     });
+    
 };
 
 document.getElementById('sort-date').addEventListener('click', () => {
