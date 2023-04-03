c8d74d9c10ba00a8d5571dda140a39c84dd5c8c0
Sumayea Rahman <shorbonam104@gmail.com>
2023-04-03 19:17:55 +0600

    see more button

--- js/script.js
+++ js/script.js
@@ -26,19 +26,19 @@ const showAi = (tools) => {
                     <i class="fa-sharp fa-solid fa-calendar-days"></i>
                     <p>${tool.published_in}</p>
                     
-<label for="my-modal-3" class="btn btn-circle btn-outline btn-accent">
-<i class="fa-solid fa-arrow-right"></i>
-</label>
+            <label for="my-modal-3" class="btn btn-circle btn-outline btn-accent">
+            <i class="fa-solid fa-arrow-right"></i>
+            </label>
 
-<!-- Put this part before </body> tag -->
-<input type="checkbox" id="my-modal-3" class="modal-toggle" />
-<div class="modal">
-  <div class="modal-box relative">
-    <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-    <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
-    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-  </div>
-</div>
+            <!-- Put this part before </body> tag -->
+            <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+            <div class="modal">
+                <div class="modal-box relative">
+                    <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
+                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                </div>
+            </div>
                     
                 </div>
             </div>
