1ceee21ccdc1405fe3fa0d54ea305415f2e25de2
Sumayea Rahman <shorbonam104@gmail.com>
2023-04-03 14:29:04 +0600

    spinner

--- js/script.js
+++ js/script.js
@@ -17,28 +17,32 @@ const showAi = (tools) => {
         const aiDiv = document.createElement('div');
         // aiDiv.classList.add('col');
         aiDiv.innerHTML = `
-    <div  class="card w-full  bg-base-100 shadow-2xl">
-            <img class="w-full h-64 px-10 pt-10" src="${tool.image}" alt="Shoes" class="rounded-xl" />
-    <div class="card-body">
-    <h1 class="card-title">Features</h1>
-    <ol>
-        ${tool.features.map((feature, index) => `<li>${index + 1}. ${feature}</li>`).join('')}
-    </ol>
-    <hr>
-    <h1 class="card-title">${tool.name}</h1>
-    <div class="flex g-4">
-            <i class="fa-sharp fa-solid fa-calendar-days"></i>
-            <p>${tool.published_in}</p>
-            <button class="btn btn-circle btn-outline btn-accent">
-        <i class="fa-solid fa-arrow-right"></i>
-        </button>
-    </div>
+        <div class="card w-full  bg-base-100 shadow-2xl">
+                    <img class="w-full h-64 px-10 pt-10" src="${tool.image}" alt="Shoes" class="rounded-xl" />
+                    <div class="card-body">
+                        <h1 class="card-title">Features</h1>
+                        <ol>
+                            ${tool.features.map((feature, index) => `<li>${index + 1}. ${feature}</li>`).join('')}
+                        </ol>
+                        <hr>
+                        <h1 class="card-title">${tool.name}</h1>
+                        <div class="flex g-4">
+                            <i class="fa-sharp fa-solid fa-calendar-days"></i>
+                            <p>${tool.published_in}</p>
+                            <button class="btn btn-circle btn-outline btn-accent ">
+                                <i class="fa-solid fa-arrow-right"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
     `
 
         toolContainer.appendChild(aiDiv);
     });
 
 };
+
+
 
 
 loadAi();
