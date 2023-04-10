7ee2d276dd49288dcaf48609106e365e0f0eb09f
Sumayea Rahman <shorbonam104@gmail.com>
2023-04-10 22:15:23 +0600

    create div

--- js/script.js
+++ js/script.js
@@ -1,87 +1,105 @@
-const loadAi = () => {
-  const URl = "https://openapi.programming-hero.com/api/ai/tools";
+// const loadAi = () => {
+//   const URl = "https://openapi.programming-hero.com/api/ai/tools";
 
-  fetch(URl)
-      .then((res) => res.json())
-      .then((data) => showAi(data.data.tools));
-};
+//   fetch(URl)
+//       .then((res) => res.json())
+//       .then((data) => showAi(data.data.tools));
+// };
 
-const showAi = (tools) => {
-  const toolContainer = document.getElementById("AI-container");
+// const showAi = (tools) => {
+//   const toolContainer = document.getElementById("AI-container");
 
-  tools.forEach((tool) => {
-      const aiDiv = document.createElement("div");
+//   tools.forEach((tool) => {
+//       const aiDiv = document.createElement("div");
 
-      aiDiv.innerHTML = `
-      <div class="card w-full  bg-base-100 shadow-2xl">
-        <img class="w-full h-64 px-10 pt-10" src="${tool.image
-          }" alt="Shoes" class="rounded-xl" />
-        <div class="card-body">
-          <h1 class="card-title">Features</h1>
-          <ol>
-            ${tool.features
-              .map((feature, index) => `<li>${index + 1}. ${feature}</li>`)
-              .join("")}
-          </ol>
-          <hr>
-          <h1 class="card-title">${tool.name}</h1>
-          <div class="flex g-4">
-            <i class="fa-sharp fa-solid fa-calendar-days"></i>
-            <p>${tool.published_in}</p>
-            <label for="my-modal-3" class="btn btn-circle btn-outline border-hidden text-red-800">
-              <i class="fa-solid fa-arrow-right"></i>
-            </label>
-            <!-- Put this part before </body> tag -->
-            <input type="checkbox" id="my-modal-3" class="modal-toggle" />
-            <div class="modal">
-              <div class="modal-box relative">
-                <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2 bg-white border-hidden text-red-800"> ✕ </label>
-                <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
-                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                <div class="modal-action">
-                  <label for="my-modal-3" class="btn bg-error border-hidden">Close!</label>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
+//       aiDiv.innerHTML = `
+//       <div class="card w-full  bg-base-100 shadow-2xl">
+//         <img class="w-full h-64 px-10 pt-10" src="${tool.image
+//           }" alt="Shoes" class="rounded-xl" />
+//         <div class="card-body">
+//           <h1 class="card-title">Features</h1>
+//           <ol>
+//             ${tool.features
+//               .map((feature, index) => `<li>${index + 1}. ${feature}</li>`)
+//               .join("")}
+//           </ol>
+//           <hr>
+//           <h1 class="card-title">${tool.name}</h1>
+//           <div class="flex g-4">
+//             <i class="fa-sharp fa-solid fa-calendar-days"></i>
+//             <p>${tool.published_in}</p>
+//             <label for="my-modal-3" class="btn btn-circle btn-outline border-hidden text-red-800">
+//               <i class="fa-solid fa-arrow-right"></i>
+//             </label>
+//             <!-- Put this part before </body> tag -->
+//             <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+//             <div class="modal">
+//               <div class="modal-box relative">
+//                 <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2 bg-white border-hidden text-red-800"> ✕ </label>
+//                 <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
+//                 <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+//                 <div class="modal-action">
+//                   <label for="my-modal-3" class="btn bg-error border-hidden">Close!</label>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     `;
 
-      toolContainer.appendChild(aiDiv);
-  });
+//       toolContainer.appendChild(aiDiv);
+//   });
 
-  // hide all the cards after the 6th card
-  const cards = Array.from(toolContainer.children);
-  cards.slice(6).forEach((card) => {
-      card.style.display = "none";
-  });
+//   // hide all the cards after the 6th card
+//   const cards = Array.from(toolContainer.children);
+//   cards.slice(6).forEach((card) => {
+//       card.style.display = "none";
+//   });
 
-  // show all the cards on "See More" button click
-  const seeMoreButton = document.getElementById("See-more");
-  seeMoreButton.addEventListener("click", () => {
-      cards.slice(6).forEach((card) => {
-          card.style.display = "block";
-      });
-      seeMoreButton.style.display = "none";
-  });
-};
+//   // show all the cards on "See More" button click
+//   const seeMoreButton = document.getElementById("See-more");
+//   seeMoreButton.addEventListener("click", () => {
+//       cards.slice(6).forEach((card) => {
+//           card.style.display = "block";
+//       });
+//       seeMoreButton.style.display = "none";
+//   });
+// };
 
-const sortCardsByDate = () => {
-  const toolContainer = document.getElementById("AI-container");
-  const cards = Array.from(toolContainer.children);
+// const sortCardsByDate = () => {
+//   const toolContainer = document.getElementById("AI-container");
+//   const cards = Array.from(toolContainer.children);
 
-  cards.sort((a, b) => {
-      const dateA = new Date(a.querySelector("p").textContent);
-      const dateB = new Date(b.querySelector("p").textContent);
+//   cards.sort((a, b) => {
+//     const dateA = new Date(a.querySelector("p").textContent);
+//     const dateB = new Date(b.querySelector("p").textContent);
 
-      return dateB - dateA;
-  });
+//     return dateB - dateA;
+//   });
 
-  toolContainer.innerHTML = '';
-  cards.forEach((card) => toolContainer.appendChild(card));
-};
+//   toolContainer.innerHTML = '';
+//   cards.forEach((card) => toolContainer.appendChild(card));
+// };
 
-document.getElementById("sort-date").addEventListener("click", sortCardsByDate);
+// document.getElementById("sort-date").addEventListener("click", sortCardsByDate);
 
-loadAi();
+
+// const seeMoreBtn = document.getElementById("See-more");
+
+// seeMoreBtn.addEventListener("click", function() {
+//   // Add spinner to the button
+//   seeMoreBtn.innerHTML = "Loading... <i class='fa fa-spinner fa-spin'></i>";
+
+//   // Set a timeout to remove the spinner after 10 seconds
+//   setTimeout(function() {
+//     seeMoreBtn.innerHTML = "See More";
+//   }, 10000);
+// });
+
+
+
+
+
+
+// loadAi();
---
+++ js/test.js
@@ -0,0 +1,58 @@
+// {
+//     // get modal and modal-details div
+//     const modal = document.getElementById("my-modal");
+//     const modalDetails = document.getElementById("modal-details");
+
+//     // set modal content
+//     modalDetails.innerHTML = `
+  
+  
+//     <div class="card lg:card-side bg-base-100 shadow-xl">
+//     <div>
+//       <h1>${data.description}</h1>
+//       <div class="flex">
+//           <p>${data.pricing}</p>
+//           <p>${data.pricing}</p>
+//           <p>${data.pricing}</p>
+//       </div>
+//       <div>
+//           <div>
+//               <h1></h1>
+//           </div>
+//       </div>
+//   <p>Price: ${data.price}</p>
+//   </div>
+//     <div class="card-body">
+//       <h2 class="card-title">New album is released!</h2>
+//       <p>Click the button to listen on Spotiwhy app.</p>
+      
+//     </div>
+//   </div>
+  
+  
+  
+  
+  
+  
+  
+  
+    
+// `;
+
+//     // display modal
+//     modal.style.display = "block";
+
+//     // close modal on clicking close button
+//     const closeBtn = document.getElementsByClassName("close")[0];
+//     closeBtn.onclick = () => modal.style.display = "none";
+
+//     // close modal on clicking anywhere outside the modal
+//     window.onclick = (event) => {
+//         if (event.target == modal) {
+//             modal.style.display = "none";
+//         }
+//     }
+// })
+// .catch(error => console.log(error));
+// }
+
