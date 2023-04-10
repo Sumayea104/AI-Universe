99c7940ba73d7e1d1b548cde79206fe397f25a66
Sumayea Rahman <shorbonam104@gmail.com>
2023-04-10 20:41:13 +0600

    css

--- js/script.js
+++ js/script.js
@@ -28,18 +28,18 @@ const showAi = (tools) => {
           <div class="flex g-4">
             <i class="fa-sharp fa-solid fa-calendar-days"></i>
             <p>${tool.published_in}</p>
-            <label for="my-modal-3" class="btn btn-circle btn-outline btn-accent">
+            <label for="my-modal-3" class="btn btn-circle btn-outline border-hidden text-red-800">
               <i class="fa-solid fa-arrow-right"></i>
             </label>
             <!-- Put this part before </body> tag -->
             <input type="checkbox" id="my-modal-3" class="modal-toggle" />
             <div class="modal">
               <div class="modal-box relative">
-                <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2 bg-white border-hidden text-red-800"> ✕ </label>
                 <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
                 <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
                 <div class="modal-action">
-                  <label for="my-modal-3" class="btn bg-error">Close!</label>
+                  <label for="my-modal-3" class="btn bg-error border-hidden">Close!</label>
                 </div>
               </div>
             </div>
