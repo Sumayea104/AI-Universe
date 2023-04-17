fa8597c2355037803abe5c87ad397454e25832dd
Sumayea Rahman <shorbonam104@gmail.com>
2023-04-17 22:37:38 +0600

    done

--- js/script.js
+++ js/script.js
@@ -32,11 +32,8 @@ const showAi = (tools) => {
                 </label>
             </div>
         </div>
-    </div>
-
-    
+    </div>    
         <section>
-
             <input type="checkbox" id="my-modal-5" class="modal-toggle" />
             <div class="modal">
                 <div class="modal-box w-11/12 max-w-5xl">
@@ -52,25 +49,15 @@ const showAi = (tools) => {
                     </div>
                 </div>
             </div>
-
         </section>
     `;
-
-
-
-        toolContainer.appendChild(aiDiv);
+    toolContainer.appendChild(aiDiv);
     });
-
-    
-
     // hide all the cards after the 6th card
     const cards = Array.from(toolContainer.children);
     cards.slice(6).forEach((card) => {
         card.style.display = "none";
     });
-
-    
-
     // show all the cards on "See More" button click
     const seeMoreButton = document.getElementById("See-more");
     seeMoreButton.addEventListener("click", () => {
@@ -81,39 +68,30 @@ const showAi = (tools) => {
     });
 };
 
-
-
-
-
 const sortCardsByDate = () => {
     const toolContainer = document.getElementById("AI-container");
     const cards = Array.from(toolContainer.children);
-
     cards.sort((a, b) => {
         const dateA = new Date(a.querySelector("p").textContent);
         const dateB = new Date(b.querySelector("p").textContent);
-
         return dateB - dateA;
     });
-
     // Display loader while the cards are being sorted
     const loader = document.getElementById("loader");
     loader.classList.remove("hidden");
-
     // Use setTimeout to simulate a delay while the cards are being sorted
     setTimeout(() => {
         toolContainer.innerHTML = '';
         cards.forEach((card) => toolContainer.appendChild(card));
-        
         // Hide the loader after the cards are sorted
         loader.classList.add("hidden");
     }, 3000);
 };
-
 document.getElementById("sort-date").addEventListener("click", sortCardsByDate);
-
-
 loadAi();
+
+
+
 // function to fetch data from API and show in modal
 const showDetails = (id) => {
     const apiUrl = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
@@ -129,72 +107,60 @@ const showDetails = (id) => {
             modalDetailsContainer.innerHTML = 'Network Problem. Please try again later.';
         });
 };
-
-
 const showDetailsData = (data) => {
     const modalDetails = document.createElement("div");
-    
     modalDetails.innerHTML = `
     <div class="flex flex-col md:flex-row ">
-    <div class="card bg-blue-100 shadow-xl flex-1 mt-4">
-        <div class="card-body">
-            <h1 class="text-2xl font-bold ">${data.description}</h1>
-            <div class="flex flex-col md:flex-row  justify-around">
-                <button class="btn btn-primary md:mr-4 my-2 w-full md:w-1/3">
-                ${data.pricing[0].price === "0" || data.pricing[0].price === "No cost"? `Free of cost/${data.pricing[0].plan === "Free" ? "Basic" : data.pricing[0].plan}`: `${data.pricing[0].price} ${data.pricing[0].plan}`}
-
-                </button>
-                <button class="btn btn-primary md:mr-4 my-2 w-full md:w-1/3">
-                ${data.pricing[1].price === "0" || data.pricing[1].price === "No cost"? `Free of cost/${data.pricing[1].plan === "Free" ? "Pro" : data.pricing[1].plan}`: `${data.pricing[1].price} ${data.pricing[1].plan}`}
-                </button>
-                <button class="btn btn-primary md:mr-4 my-2 w-full md:w-1/3">
-                ${data.pricing[2].price === "0" || data.pricing[2].price === "No cost"? `Free of cost/${data.pricing[2].plan === "Free" ? "Enterprise" : data.pricing[2].plan}`: `${data.pricing[2].price} ${data.pricing[2].plan}`}
-                </button>
-            </div>
-            <div class="flex flex-col md:flex-row  my-4 justify-around">
-                <div>
-                    <h1 class="card-title">Features</h1>
-                    <ul>
-                        ${Object.values(data.features)
+        <div class="card bg-blue-100 shadow-xl flex-1 mt-4">
+            <div class="card-body">
+                <h1 class="text-2xl font-bold ">${data.description}</h1>
+                <div class="flex flex-col md:flex-row  justify-around">
+                    <button class="btn btn-primary md:mr-4 my-2 w-full md:w-1/3">
+                    ${data.pricing[0].price === "0" || data.pricing[0].price === "No cost"? `Free of cost/${data.pricing[0].plan === "Free" ? "Basic" : data.pricing[0].plan}`: `${data.pricing[0].price} ${data.pricing[0].plan}`}
+                    </button>
+                    <button class="btn btn-primary md:mr-4 my-2 w-full md:w-1/3">
+                    ${data.pricing[1].price === "0" || data.pricing[1].price === "No cost"? `Free of cost/${data.pricing[1].plan === "Free" ? "Pro" : data.pricing[1].plan}`: `${data.pricing[1].price} ${data.pricing[1].plan}`}
+                    </button>
+                    <button class="btn btn-primary md:mr-4 my-2 w-full md:w-1/3">
+                    ${data.pricing[2].price === "0" || data.pricing[2].price === "No cost"? `Free of cost/${data.pricing[2].plan === "Free" ? "Enterprise" : data.pricing[2].plan}`: `${data.pricing[2].price} ${data.pricing[2].plan}`}
+                    </button>
+                </div>
+                <div class="flex flex-col md:flex-row  my-4 justify-around">
+                    <div>
+                        <h1 class="card-title mt-4">Features</h1>
+                        <ul>
+                            ${Object.values(data.features)
                             .map(feature => `<li>&bull; ${feature.feature_name}</li>`)
                             .join("")}
-                    </ul>
-                </div>
-                <div>
-                    <h1 class="card-title">Integrations</h1>
-                    ${Object.keys(data.integrations).length > 0
-                        ? `<ul>${Object.values(data.integrations)
+                        </ul>
+                    </div>
+                    <div>
+                        <h1 class="card-title mt-4">Integrations</h1>
+                            ${Object.keys(data.integrations).length > 0
+                            ? `<ul>${Object.values(data.integrations)
                             .map(integration => `<li>&bull; ${integration}</li>`)
                             .join("")}</ul>`
-                        : "<p>No Data Found</p>"}
-                    
+                            : "<p>No Data Found</p>"}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="card bg-yellow-100 shadow-xl flex-1 mt-4 sm:ml-4">
-    <div class="card-body">
-        <div class="relative">
-            <img class="w-full h-64 rounded-xl" src="${data.image_link[0]}" alt="ChatGPT" />
-            ${data.accuracy.score > 0 ? `<div class="absolute top-0 right-0 bg-red-500 text-white p-2 rounded-bl-lg font-bold">${data.accuracy.score * 100}% accuracy</div>` : ""}
-
+        <div class="card bg-yellow-100 shadow-xl flex-1 mt-4 sm:ml-4">
+            <div class="card-body">
+                <div class="relative">
+                    <img class="w-full h-64 rounded-xl" src="${data.image_link[0]}" alt="ChatGPT" />
+                        ${data.accuracy.score > 0 ? `<div class="absolute top-0 right-0 bg-red-500 text-white p-2 rounded-bl-lg font-bold">${data.accuracy.score * 100}% accuracy</div>` : ""}
+                </div>
+                    <p>
+                        ${data.input_output_examples.length > 0 ?
+                        `<strong>${data.input_output_examples[0].input}</strong> <br>
+                        ${data.input_output_examples[0].output}` :
+                        "NO, not yet! Take a break."}
+                    </p>
+            </div>
         </div>
-        <p>
-        ${data.input_output_examples.length > 0 ?
-            `<strong>${data.input_output_examples[0].input}</strong> <br>
-            ${data.input_output_examples[0].output}` :
-            "NO, not yet! Take a break."}
-        
-        </p>
     </div>
-</div>
-
-    </div>
-</div>
-
     `;
-
-
     const modalDetailsContainer = document.getElementById("modal-details");
     modalDetailsContainer.innerHTML = '';
     modalDetailsContainer.appendChild(modalDetails);
